fe5ce2750f08a1320795b9cf7b2158b13485f530
Esteban Camargo <estebmaister@gmail.com>
2020-06-15 01:36:38 -0400

    :zap: connection updated & config created

---
+++ app/config.js
@@ -0,0 +1,22 @@
+'use strict';
+
+require('dotenv').config();
+
+const SESSION_SECRET = process.env.SESSION_SECRET;
+const MONGO_URI = process.env.MONGO_URI;
+const PORT = process.env.PORT;
+const GITHUB_CLIENT_ID =
+	process.env.LOCAL_CLIENT_ID || process.env.GITHUB_CLIENT_ID;
+const GITHUB_CLIENT_SECRET =
+	process.env.LOCAL_CLIENT_SECRET || process.env.GITHUB_CLIENT_SECRET;
+const GITHUB_CALLBACK_URL =
+	process.env.P_URL || 'http://127.0.0.1:3000/auth/github/callback';
+
+module.exports = {
+	SESSION_SECRET,
+	MONGO_URI,
+	PORT,
+	GITHUB_CLIENT_ID,
+	GITHUB_CLIENT_SECRET,
+	GITHUB_CALLBACK_URL,
+};
--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 'use strict';
 
-const express     = require('express');
-const session     = require('express-session');
-const bodyParser  = require('body-parser');
-const fccTesting  = require('./freeCodeCamp/fcctesting.js');
-const auth        = require('./app/auth.js');
-const routes      = require('./app/routes.js');
-const mongo       = require('mongodb').MongoClient;
-const passport    = require('passport');
-const cookieParser= require('cookie-parser')
-const app         = express();
-const http        = require('http').Server(app);
-const sessionStore= new session.MemoryStore();
-
+const express = require('express');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const auth = require('./app/auth.js');
+const routes = require('./app/routes.js');
+const mongoClient = require('mongodb').MongoClient;
+const passport = require('passport');
+const cookieParser = require('cookie-parser');
+const app = express();
+const http = require('http').Server(app);
+const sessionStore = new session.MemoryStore();
+const io = require('socket.io')(http);
+const config = require('./app/config.js');
 
 fccTesting(app); //For FCC testing purposes
 
@@ -20,31 +21,37 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.set('view engine', 'pug')
+app.set('view engine', 'pug');
 
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: true,
-  saveUninitialized: true,
-  key: 'express.sid',
-  store: sessionStore,
-}));
+app.use(
+	session({
+		secret: config.SESSION_SECRET,
+		resave: true,
+		saveUninitialized: true,
+		key: 'express.sid',
+		store: sessionStore,
+	})
+);
+const client = new mongoClient(config.MONGO_URI, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
+client.connect((err) => {
+	if (err) console.log('Database error: ' + err);
+	const db = client.db('test');
+	console.log('Successful database connection to: ' + db.s.namespace);
 
+	auth(app, db);
+	routes(app, db);
 
-mongo.connect(process.env.DATABASE, (err, db) => {
-    if(err) console.log('Database error: ' + err);
-  
-    auth(app, db);
-    routes(app, db);
-      
-    http.listen(process.env.PORT || 3000);
+	const listener = http.listen(config.PORT || 3001, 'localhost', () => {
+		const { address, port } = listener.address();
+		console.log(`Server is listening at http://${address}:${port}`);
+	});
 
-  
-    //start socket.io code  
-
-  
-
-    //end socket.io code
-  
-  
+	//start socket.io code
+	io.on('connection', (socket) => {
+		console.log('A user has connected');
+	});
+	//end socket.io code
 });
