1e7a5837579242b6e913a1fd110d085adfbd0cef
Esteban Camargo <estebmaister@gmail.com>
2020-06-18 13:51:48 -0400

    :sparkles: Adding status of users in client side

--- public/client.js
+++ public/client.js
@@ -1,6 +1,18 @@
 $(document).ready(() => {
-	var socket = io();
+	const socket = io();
+
 	socket.on('user count', (data) => console.log(data));
+
+	socket.on('user', (data) => {
+		$('#num-users').text(data.currentUsers + ' users online');
+		let message = data.name;
+		if (data.connected) {
+			message += ' has joined the chat.';
+		} else {
+			message += ' has left the chat.';
+		}
+		$('#messages').append($('<li>').html('<b>' + message + '</b>'));
+	});
 
 	// Form submittion with new message in field with id 'm'
 	$('form').submit(() => {
