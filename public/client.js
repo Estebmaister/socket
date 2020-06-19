6a7ed31c81322890d7802a7f787e959e775a9a76
Esteban Camargo <estebmaister@gmail.com>
2020-06-19 02:35:36 -0400

    :zap: Compressing functions

--- public/client.js
+++ public/client.js
@@ -1,24 +1,20 @@
 $(document).ready(() => {
-	const socket = io();
+	/*global io*/
+	var socket = io();
 
 	socket.on('user', (data) => {
 		$('#num-users').text(data.currentUsers + ' users online');
 		let message = data.name;
-		if (data.connected) {
-			message += ' has joined the chat.';
-		} else {
-			message += ' has left the chat.';
-		}
+		if (data.connected) message += ' has joined the chat.';
+		if (!data.connected) message += ' has left the chat.';
 		$('#messages').append($('<li>').html('<b>' + message + '</b>'));
 	});
 
 	socket.on('user count', (data) => console.log(data));
 
-	socket.on('chat message', (data) => {
-		$('#num-users').text(data.currentUsers + ' users online');
-		let message = `${data.name}: ${data.message}`;
-		$('#messages').append($('<li>').html('<b>' + message + '</b>'));
-	});
+	socket.on('chat message', (data) =>
+		$('#messages').append($('<li>').text(`${data.name}: ${data.message}`))
+	);
 
 	// Form submittion with new message in field with id 'm'
 	$('form').submit(() => {
